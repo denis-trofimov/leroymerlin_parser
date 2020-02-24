7e3a13446c9d0bfe409f6a15efa851e0d100e25c
Denis Trofimov <silaradost@yandex.ru>
2020-02-25 02:03:31 +0300

    test: add test_parse_products_page

--- parse.py
+++ parse.py
@@ -39,19 +39,38 @@ def parse_sub_category_page(url):
     if not responce.ok:
         return []
 
+    return parse_products_cards(responce.content, url)
+
+
+def parse_products_cards(content, baseurl):
+    """Parse product cards
+
+    <div class="ui-product-card" comparison-checked="false" 
+    shopping-list-checked="false" data-product-web-saleable="true" 
+    data-product-url="/product/shtukaturka-gipsovaya-knauf-rotband-30-kg-10073940/" 
+    data-product-category-id="shtukaturki-201709_Opus_Family" 
+    data-product-price="418.00" data-sub-category-id="20" data-category-id="65" 
+    data-product-material="Гипс" data-product-gamma="A" data-unit="NIU" 
+    data-division-id="1" data-source="Step" data-product-color="Серый" 
+    data-product-stock-value="1196" data-product-has-linked-how-tos="0" 
+    data-product-location="Catalog" data-product-id="10073940" 
+    data-product-dimension65="STD" data-product-weight="30" 
+    data-rel="js-cat-product-item" data-product-brand="KNAUF" data-place="plp" 
+    data-element-id="ui-product-card" data-ga-root="data-ga-root" 
+    data-sub-division-id="110" 
+    data-product-name="Штукатурка гипсовая Knauf Ротбанд 30 кг">
+    """
     products = []
     unread_count = 0
-    soup = BeautifulSoup(responce.content, 'html.parser')
+    soup = BeautifulSoup(content, 'html.parser')
     cards = soup.select('div.ui-sorting-cards')
 
     for card in cards:
-        """
-        <div class="ui-product-card" comparison-checked="false" shopping-list-checked="false" data-product-web-saleable="true" data-product-url="/product/shtukaturka-gipsovaya-knauf-rotband-30-kg-10073940/" data-product-category-id="shtukaturki-201709_Opus_Family" data-product-price="421.00" data-sub-category-id="20" data-category-id="65" data-product-material="Гипс" data-product-gamma="A" data-unit="NIU" data-division-id="1" data-source="Step" data-product-color="Серый" data-product-stock-value="1175" data-product-has-linked-how-tos="0" data-product-location="Catalog" data-product-id="10073940" data-product-dimension65="STD" data-product-weight="30" data-rel="js-cat-product-item" data-product-brand="KNAUF" data-place="plp" data-element-id="ui-product-card" data-ga-root="data-ga-root" data-sub-division-id="110" data-product-name="Штукатурка гипсовая Knauf Ротбанд 30 кг" shop-cart-checked="false">
-        """
+
         product = card.select_one('div.ui-product-card')
         name = product.get('data-product-name')
         link = product.get('data-product-url')
-        link = urljoin(url, link)
+        link = urljoin(baseurl, link)
         price = product.get('data-product-price')
         print(name, price, link)
 
