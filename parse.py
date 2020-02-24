e7ce88c2d3c1d54a7bff7fb9644555ed91e7e595
Denis Trofimov <silaradost@yandex.ru>
2020-02-25 00:40:34 +0300

    ref: good to product

--- parse.py
+++ parse.py
@@ -4,8 +4,8 @@ from urllib.parse import urljoin
 from collections import namedtuple
 
 
-"A new custom goods tuple"
-Good = namedtuple("Good", ('name', 'link', 'price'))
+"A new custom products tuple"
+Product = namedtuple("Product", ('name', 'link', 'price'))
 
 
 def parse_handler(url, listSelector, textSelector, linkSelector):
@@ -36,7 +36,7 @@ def parse_handler(url, listSelector, textSelector, linkSelector):
 def parse_goods_page(url):
     "parse_goods_page is a new custom goods page parser"
 
-    goods = []
+    products = []
     unreadCnt = 0
 
     page = requests.get(url)
@@ -47,14 +47,14 @@ def parse_goods_page(url):
         """
         <div class="ui-product-card" comparison-checked="false" shopping-list-checked="false" data-product-web-saleable="true" data-product-url="/product/shtukaturka-gipsovaya-knauf-rotband-30-kg-10073940/" data-product-category-id="shtukaturki-201709_Opus_Family" data-product-price="421.00" data-sub-category-id="20" data-category-id="65" data-product-material="Гипс" data-product-gamma="A" data-unit="NIU" data-division-id="1" data-source="Step" data-product-color="Серый" data-product-stock-value="1175" data-product-has-linked-how-tos="0" data-product-location="Catalog" data-product-id="10073940" data-product-dimension65="STD" data-product-weight="30" data-rel="js-cat-product-item" data-product-brand="KNAUF" data-place="plp" data-element-id="ui-product-card" data-ga-root="data-ga-root" data-sub-division-id="110" data-product-name="Штукатурка гипсовая Knauf Ротбанд 30 кг" shop-cart-checked="false">
         """
-        good = card.select_one('div.ui-product-card')
-        name = good.get('data-product-name')
-        link = good.get('data-product-url')
+        product = card.select_one('div.ui-product-card')
+        name = product.get('data-product-name')
+        link = product.get('data-product-url')
         link = urljoin(url, link)
-        price = good.get('data-product-price')
+        price = product.get('data-product-price')
         print(name, price, link)
 
-        goods.append(Good(name=name, link=link, price=price))
+        products.append(Product(name=name, link=link, price=price))
 
     print('unread {} rows'.format(unreadCnt))
-    return goods
+    return products
